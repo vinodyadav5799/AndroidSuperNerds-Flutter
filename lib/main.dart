f56ab8949c35c17c5a0b520b841f177919fa6bad
vinod yadav <yadav.vinod579@gmail.com>
2020-10-18 14:20:34 +0530

    removal of debug banner from top right
    removal of debug banner from the top right of the app and implemented fat arrow in the main method.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() => runApp(new FlutterApp());
 
-  runApp(new FlutterApp());
-}
 
 class FlutterApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
          title: "My First Flutter App",
          home: Scaffold(
            appBar: AppBar(title: Text("Home Screen"), backgroundColor: Colors.lightGreen,),
