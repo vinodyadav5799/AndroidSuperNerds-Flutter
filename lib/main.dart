cde01081f079846af716b1ed99fb6fb54da498b8
vinod yadav <yadav.vinod579@gmail.com>
2020-10-18 13:12:43 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(
+    Center(
+      child:
+      Text(
+        "Hello Guys Welcome to AndroidSuperNerds",
+        textDirection: TextDirection.ltr
+      ),
+    )
+  );
+}
