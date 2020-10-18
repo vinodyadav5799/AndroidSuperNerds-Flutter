05fc95cb412ecbd16f2862855acc8fe03e803ff7
vinod yadav <yadav.vinod579@gmail.com>
2020-10-18 13:52:34 +0530

    Updated the main dart file with App bar integration
    Implemented Scafford Widget with AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,20 @@ void main(){
 
     MaterialApp(
       title: "My First Flutter App",
-      home: Material(
-        color: Colors.lightGreenAccent,
-        child: Center(
-          child: Text(
-            "Hi Friends",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 40.0),
-          ),
-        )
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Screen"),
+          backgroundColor: Colors.lightGreen,),
+        body: Material(
+            color: Colors.lightGreenAccent,
+            child: Center(
+              child: Text(
+                "Hi Friends",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 40.0),
+              ),
+            )
+        ),
       )
     )
   );
