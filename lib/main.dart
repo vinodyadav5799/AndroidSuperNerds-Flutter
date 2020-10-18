3d4c3482b1cc117ce4328f13bedc3d1adc1e341f
vinod yadav <yadav.vinod579@gmail.com>
2020-10-18 14:14:36 +0530

    StatelessWidget integration
    Code organising with Stateless Widget integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,29 @@ import 'package:flutter/material.dart';
 
 void main(){
 
-  runApp(
+  runApp(new FlutterApp());
+}
 
-    MaterialApp(
-      title: "My First Flutter App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Home Screen"),
-          backgroundColor: Colors.lightGreen,),
-        body: Material(
-            color: Colors.lightGreenAccent,
-            child: Center(
+class FlutterApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+         title: "My First Flutter App",
+         home: Scaffold(
+           appBar: AppBar(title: Text("Home Screen"), backgroundColor: Colors.lightGreen,),
+           body: Material(
+             color: Colors.lightGreenAccent,
+             child: Center(
               child: Text(
-                "Hi Friends",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.white, fontSize: 40.0),
+                  "Hi Friends",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(color: Colors.white, fontSize: 40.0),
               ),
-            )
-        ),
-      )
-    )
-  );
+             )
+           ),
+         )
+    );
+  }
+
 }
