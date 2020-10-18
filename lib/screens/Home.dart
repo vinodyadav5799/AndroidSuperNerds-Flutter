0c5cc1c98fd9fbc6a8f54117e65e568ade65bdc3
vinod yadav <yadav.vinod579@gmail.com>
2020-10-19 00:33:57 +0530

    style and custom font implementation
    All text style and custom font integration.

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 
@@ -12,9 +14,26 @@ class Home extends StatelessWidget{
         color: Colors.lightGreenAccent,
         // width: 200.0,
         // height: 100.0,
-        margin: EdgeInsets.all(40.0),
-        padding: EdgeInsets.only(left:45.0),
-        child: Text("Hi Guys", textDirection: TextDirection.ltr),
+        // margin: EdgeInsets.all(40.0),
+        // padding: EdgeInsets.only(left:45.0),
+        child: Text(
+          "Hi Guys",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 30.0,
+            fontFamily: 'Roboto',
+            color: Colors.orange,
+
+            // for italic
+            // fontWeight: FontWeight.w300,
+            // fontStyle: FontStyle.italic,
+
+            // for bold
+            fontWeight: FontWeight.w700,
+
+          ),
+        ),
       ),
     );
   }
