9f6b8e48dd3d9558a2ac6128996eaa549aa442ab
vinod yadav <yadav.vinod579@gmail.com>
2020-10-18 19:23:14 +0530

    return type method integration
    Return type method implementation

--- lib/screens/FirstScreen.dart
+++ lib/screens/FirstScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatelessWidget{
@@ -7,12 +9,18 @@ class FirstScreen extends StatelessWidget{
           color: Colors.lightGreenAccent,
           child: Center(
             child: Text(
-              "Hi Friends",
+              "Your Random number is ${getRandomNumber()}",
               textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 40.0),
+              style: TextStyle(color: Colors.white, fontSize: 30.0),
             ),
           )
       );
+  }
+
+  int getRandomNumber(){
+    var number = Random();
+    int randomNumber = number.nextInt(10);
+    return randomNumber;
   }
 
 }
